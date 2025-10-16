f4d9195e9fa70586221737c831fde500d9e790ca
John Bicierro <zrkeimperial@gmail.com>
2025-10-16 08:30:20 +0800

    feat: implement GET API route for fetching advertisements

---
+++ src/app/api/ads/route.ts
@@ -0,0 +1,16 @@
+import { db } from "@/db";
+import { advertistments } from "@/db/schema";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const data = await db.select().from(advertistments).limit(50);
+    return NextResponse.json({ advertistments: data }, { status: 200 });
+  } catch (err) {
+    console.error("Error in GET /api/posts:", err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
