cdcf88e3459379a547726b1f93eed4f8d8a03cc7
John Bicierro <zrkeimperial@gmail.com>
2025-10-16 08:38:45 +0800

    fix: correct spelling errors in advertisements schema and API response

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
 
-        <Toaster />
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );
