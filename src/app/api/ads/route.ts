a4346304510edd0554d43c09c8431f22b7eb4803
John Bicierro <zrkeimperial@gmail.com>
2025-10-16 08:31:53 +0800

    fix: correct error logging message in GET API for advertisements

--- src/app/api/ads/route.ts
+++ src/app/api/ads/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
     const data = await db.select().from(advertistments).limit(50);
     return NextResponse.json({ advertistments: data }, { status: 200 });
   } catch (err) {
-    console.error("Error in GET /api/posts:", err);
+    console.error("Error in GET /api/advertistments:", err);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 },
